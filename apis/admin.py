9e31729263693b3f7a57a272b958b7168f92f5ab
bistaprapannabista <bistaprapannabista@gmail.com>
2023-09-22 11:55:18 +0545

    services

--- apis/admin.py
+++ apis/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Province, District, LocalGovernment
-
+from .models import Province, District, LocalGovernment, Document, Service
+import json
 
 admin.site.register(Province)
 
@@ -15,3 +15,19 @@ class LocalGovernmentAdmin(admin.ModelAdmin):
     search_fields = ['name']
     
 admin.site.register(LocalGovernment,LocalGovernmentAdmin)
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+admin.site.register(Document,DocumentAdmin)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ["name","documents_list","price","time","responsible_person"]
+    search_fields = ["name"]
+
+    def documents_list(self,obj):
+        return ", ".join([document.name for document in obj.documents.all()])
+
+admin.site.register(Service,ServiceAdmin)
--- apis/models.py
+++ apis/models.py
@@ -25,3 +25,20 @@ class LocalGovernment(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Service(models.Model):
+    name = models.CharField(max_length=255)
+    documents = models.ManyToManyField(Document)
+    price = models.CharField(max_length=255)
+    time = models.CharField(max_length=255)
+    responsible_person = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
